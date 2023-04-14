925b2bd5a7895fb8a90298b89fb4a4e66c09e460
Marcelo Tadini <tadini.marcelo53@gmail.com>
2023-04-14 18:43:06 -0300

    First commit

---
+++ src/Album.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+
+function Album() {
+
+    const [loading, setLoading] = useState(false)
+    const [albums, setAlbums] = useState([])
+
+    useEffect(() => {
+        setLoading(true)
+        fetch('https://jsonplaceholder.typicode.com/albums')
+            .then(response => response.json())
+            .then(data => {
+                setAlbums(data)
+                setLoading(false)
+            })
+
+    }, [])
+
+    return (
+        <>
+            <h1>Álbuns</h1>
+            {
+                loading === true ? 'Carregando...' : ''
+            }
+
+            {
+                albums.map(album => {
+                    return(
+                        <div>
+                            <span>{album.title} - id: {album.id}</span>
+                        </div>
+                    )
+                })
+            }
+        </>
+    )
+}
+
+export default Album
